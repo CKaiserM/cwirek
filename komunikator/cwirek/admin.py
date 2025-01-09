662cab1e4c5b1dfb1777ed0afa1abe545a964d69
CKaiserM <kaiser.mariusz@gmail.com>
2025-01-09 23:37:19 +0100

    project cwirek

---
+++ komunikator/cwirek/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
+#unregister groups
+admin.site.unregister(Group)
+
+# Combine profile info and user
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+# Extend user model
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    #display only username
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+# Unregister initial User
+admin.site.unregister(User)
+
+# Register user and profile
+admin.site.register(User, UserAdmin)
+#admin.site.register(Profile)
+
+
---
+++ komunikator/cwirek/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CwirekConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cwirek'
