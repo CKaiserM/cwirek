319d2c5671b0ef0e49bb59f8532ac58e32647983
CKaiserM <kaiser.mariusz@gmail.com>
2025-01-10 18:56:34 +0100

    register page added

--- komunikator/cwirek/views.py
+++ komunikator/cwirek/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Yard
-from .forms import YardForm
+from .forms import YardForm, SignUpForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 # Home view
 
 def home(request):
@@ -77,4 +79,21 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #first_name = form.cleaned_data['first_name']
+            #second_name = form.cleaned_data['second_name']
+            #email = form.cleaned_data['email']
+            
+            # Log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have successfully registered"))
+            return redirect('home')
+        
+    return render(request, 'register.html', {'form':form})
